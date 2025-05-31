cd7aa1cfe529f2f3b35a129aa1589768c228d967
ICT <brandongrey1324@outlook.com>
2025-05-31 14:56:26 -0500

    02_added_credentials

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -12,14 +12,12 @@ function createWindow () {
   win.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow();
+app.whenReady().then(createWindow);
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
